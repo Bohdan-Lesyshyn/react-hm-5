7f7eb1b8aea8aa476a6992a0d960ba60b38fc5db
Bohdan_Lesyshyn <amakhasla2@gmail.com>
2021-03-27 11:12:29 +0200

    homework5

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Menu = (props)=>(
+   <nav>
+       <ul>
+           <li>{props.g}</li>
+           <li>{props.w}</li>
+           <li>{props.a}</li>
+           <li>{props.c}</li>
+           <li>{props.s}</li>
+       </ul>
+   </nav> 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Menu greeting='hello, check my menu' work='work' about='about' con='contacts' sth='sth else'/>, document.getElementById("root"));
